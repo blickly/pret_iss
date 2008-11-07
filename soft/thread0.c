598e9937d9ca10c70a893c7683f15cd7a2578270
hiren <hiren@2fe1bebc-546c-4154-8a90-0a651ce2c4e0>
2008-11-07 03:36:49 +0000

    write_error tests fixed: problem due to compile commands
    git-svn-id: svn+ssh://source.eecs.berkeley.edu/home/svn/chess/pret/trunk/pret_iss@1159 2fe1bebc-546c-4154-8a90-0a651ce2c4e0

--- soft/thread0.c
+++ soft/thread0.c
@@ -1,80 +1,15 @@
-
-/*----------------------------------------------------------------------
- *  WCET Benchmark created by Andreas Ermedahl, Uppsala university, 
- *  May 2000.  
- *
- *  The purpose of this benchmark is to have two loop where the inner 
- *  loops max number of iterations depends on the outer loops current 
- *  iterations.  The results corresponds to something Jannes flow-analysis 
- *  should produce.
- *
- * The example appeard for the first time in: 
- * 
- *  @InProceedings{Ermedahl:Annotations,
- * author =       "A. Ermedahl and J. Gustafsson",
- * title =        "Deriving Annotations for Tight Calculation of Execution Time",
- * year =         1997,
- * month =        aug,
- * booktitle =    EUROPAR97,
- * publisher =    "Springer Verlag",
- * pages =        "1298-1307"
- * }
- *
- * The result of Jannes tool is something like:
- *   outer loop:       1   2   3   4   5   6   7   8   9   10   11 
- *   inner loop max:   5   9   8   7   4   2   1   1   1   1    1 
- *
- *----------------------------------------------------------------------*/
-
-#define DEBUG 1
-#define END_SIM \
-  asm(".word 0x22222222");
-
-#ifdef DEBUG
-#include <stdio.h>
-#endif
-
-int complex(int a, int b)
-{
-  while(a < 30)
-    {
-      while(b < a)
-	{ 
-	  if(b > 5) 
-	    b = b * 3; 
-	  else
-	    b = b + 2;
-	  if(b >= 10 && b <= 12) 
-	    a = a + 10;
-	  else 
-	    a = a + 1;
-	}
-      a = a + 2; 
-      b = b - 10; 
-#ifdef DEBUG
-  printf("(a,b) = (%d,%d)\n",a,b);
-#endif 
-
-    }
-  return 1;
-}
+#include "deadline.h"
 
 int main()
-{   
-  /* a = [1..30] b = [1..30] */ 
-  int a = 1, b = 1, answer = 0;
-  /* if(answer)
-     {a = 1; b = 1;}
-     else
-     {a = 30; b = 30;} */
-
-    answer = complex(a, b);
-#ifdef DEBUG
-    //printf("Answer for (a,b)=(%d,%d) is %d\n",a,b,answer);
-#endif
-    END_SIM;
-    return answer;
-  }
-
-
-
+{
+  WRITE('c');
+  WRITEFILE('c');
+  register int i;
+  register int j;
+  for (i = 0; i < 2; ++i ) {
+    for (j = 0; j < 10; ++j);
+  } 
+  WRITE('\n');
+  END_SIMULATION;
+  return 0;
+}
--- tests/tests/pret/dma/write_error/write_error/thread_common.makefile
+++ tests/tests/pret/dma/write_error/write_error/thread_common.makefile
@@ -17,9 +17,9 @@ $(DUMP): $(EXE)
 
 $(EXE): $(C_FILES:.c=.o) 
 	# This is more lightweight by not linking in any libraries
-	$(LD) $(LDFLAGS) $^ -o $@ -e main
+	#	$(LD) $(LDFLAGS) $^ -o $@ -e main
 	# This is a normal configuration that supports libraries
-#	$(CC) $(CCFLAGS) $(INCDIR) $(LIBDIR) $^ -o $@ $(LIBS)
+	$(CC) $(CCFLAGS) $(INCDIR) $(LIBDIR) $^ -o $@ $(LIBS)
 
 .c.o:
 	$(CC) $(CCFLAGS) $(INCDIR) -c $<
