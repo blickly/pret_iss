c4e32a2884a6b4e23724a81a66b51b49cfe3b6b5
hiren <hiren@2fe1bebc-546c-4154-8a90-0a651ce2c4e0>
2008-11-24 23:29:58 +0000

    Separated special register class.
    git-svn-id: svn+ssh://source.eecs.berkeley.edu/home/svn/chess/pret/trunk/pret_iss@1217 2fe1bebc-546c-4154-8a90-0a651ce2c4e0

---
+++ include/special_registers.h
@@ -0,0 +1,58 @@
+/*
+  Copyright (c) 2007-2008 The Regents of the University of California.
+  All rights reserved.
+
+  Permission is hereby granted, without written agreement and without
+  license or royalty fees, to use, copy, modify, and distribute this
+  software and its documentation for any purpose, provided that the
+  above copyright notice and the following two paragraphs appear in all
+  copies of this software.
+
+  IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+  FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+  ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+  THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+  SUCH DAMAGE.
+
+  THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+  PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+  CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+  ENHANCEMENTS, OR MODIFICATIONS.
+
+  PT_COPYRIGHT_VERSION_2
+  COPYRIGHTENDKEY
+
+  $Id$
+
+*/
+
+
+#ifndef _SPECIAL_REGISTERS_H_
+#define _SPECIAL_REGISTERS_H_
+
+#ifndef _NO_SYSTEMC_
+#include <systemc.h>
+#lese
+#include "no_systemc.h"
+#endif
+
+#include "instruction_defs.h"
+
+class special_registers {
+public:
+  WINDOW_POINTER get_window_pointer() const {
+    return wp;
+  }
+  
+  void set_window_pointer(const WINDOW_POINTER& wp_val) {
+    wp = wp_val;
+  }
+    void reset_window_pointer() {
+        wp = 0;
+    }
+private:
+  WINDOW_POINTER wp;
+};
+#endif /* _SPECIAL_REGISTERS_H_ */
