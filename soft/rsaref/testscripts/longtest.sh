f9a6bc396615f02f764b6dded87c85ead5a4dfa1
liuisaac <liuisaac@2fe1bebc-546c-4154-8a90-0a651ce2c4e0>
2008-12-06 13:59:54 +0000

    added test scripts
    git-svn-id: svn+ssh://source.eecs.berkeley.edu/home/svn/chess/pret/trunk/pret_iss@1286 2fe1bebc-546c-4154-8a90-0a651ce2c4e0

---
+++ soft/rsaref/testscripts/longtest.sh
@@ -0,0 +1,23 @@
+if [ $# -ne 2 ]; then
+    echo "Usage: runtest.sh <# of data points> <plotout>";
+    exit;
+fi
+CURDIR=`pwd`
+RDEMODIR="$CURDIR/../src/rdemo"
+KEYDIR="$CURDIR/../src/bin/keys"
+PRETDIR="$CURDIR/.."
+datetime=`date +%Y%m%d%H%M%S`
+
+echo "Data" > data_$datetime.raw
+
+#Create encrypt.c
+for (( i = 0 ; i < $1 ; i++ ))
+do
+  cp $RDEMODIR/encryptbase.c $RDEMODIR/encrypt.c
+  cat $KEYDIR/key0512-$i >> $RDEMODIR/encrypt.c 
+  cd $PRETDIR
+  make
+  ./run.sh pret >> data_$datetime.raw
+  touch progress$i
+done
+
