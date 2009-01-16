cac85842e0b6c154c431b03c7a1a7b973be4ae47
blickly <blickly@2fe1bebc-546c-4154-8a90-0a651ce2c4e0>
2009-01-16 22:01:12 +0000

    Removed need for release to use autotools
    git-svn-id: svn+ssh://source.eecs.berkeley.edu/home/svn/chess/pret/trunk/pret_iss@1464 2fe1bebc-546c-4154-8a90-0a651ce2c4e0

--- ship.sh
+++ ship.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/make -f
 SHIP_DIR=../pret_ship
 
-ship:
+ship: configure
 # Make shipping directory
 	mkdir ${SHIP_DIR}
 	-cp -r . ${SHIP_DIR}
@@ -17,13 +17,14 @@ ship:
 	-cp -r soft/graphic ${SHIP_DIR}/examples/
 # Remove .svn and other directories.
 	-rm -rf ${SHIP_DIR}/tests/ ${SHIP_DIR}/soft/ ${SHIP_DIR}/adm/ \
-    ${SHIP_DIR}/autom4te.cache/ ${SHIP_DIR}/build-aux/ ${SHIP_DIR}/logs/ \
-    ${SHIP_DIR}/oldfiles/ ${SHIP_DIR}/format_code.sh ${SHIP_DIR}/ship.sh
-
+    ${SHIP_DIR}/autom4te.cache/ ${SHIP_DIR}/logs/ \
+    ${SHIP_DIR}/oldfiles/ ${SHIP_DIR}/aclocal.m4 ${SHIP_DIR}/config.h.in \
+    ${SHIP_DIR}/format_code.sh ${SHIP_DIR}/ship.sh \
+    ${SHIP_DIR}/bootstrap.sh ${SHIP_DIR}/configure.in
 	-find ${SHIP_DIR} -name ".svn" | xargs rm -rf	 
 	-find ${SHIP_DIR} -name "*~" | xargs rm -f
 	-mv ${SHIP_DIR}/Makefile.dist.am ${SHIP_DIR}/Makefile.am
 	-mv ${SHIP_DIR}/configure.dist.in ${SHIP_DIR}/configure.in
     
-
-
+configure:
+	./bootstrap.sh
