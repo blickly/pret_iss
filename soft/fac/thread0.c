876e235fc3ae94ad4ae4626664de28c769507330
ddd2121 <ddd2121@2fe1bebc-546c-4154-8a90-0a651ce2c4e0>
2009-01-16 22:43:23 +0000

    changes made after a fix as been made to the csim so it doesnt time out
    git-svn-id: svn+ssh://source.eecs.berkeley.edu/home/svn/chess/pret/trunk/pret_iss@1465 2fe1bebc-546c-4154-8a90-0a651ce2c4e0

--- soft/fac/thread0.c
+++ soft/fac/thread0.c
@@ -1,41 +1,83 @@
-/* MDH WCET BENCHMARK SUITE. File version $Id: thread0.c 757 2008-07-11 00:15:11Z blickly $ */
-/*
- * Changes: CS 2006/05/19: Changed loop bound from constant to variable.
- */
 
-#define DEBUG 1
-#define END_SIM \
-  asm(".word 0x22222222");
+#include"mmc_init.c"
+//#include"globals.h"
+#include<stdio.h>
+#include"error.h"
 
-#ifdef DEBUG 
-#include <stdio.h>
-#endif
+//#define END_SIM \
+  //asm(".word 0x22222222");
 
+unsigned int Transaction_speed;
+unsigned int Access_time;
+void intialize_global_parameters();
 
+int main(){
+	intialize_global_parameters();
+	ErrorCode_t Error= NO_ERROR;
+	
+	printf("GO_TO_IDLE_STATE\n");
+	Error=SEND_GO_TO_IDLE_STATE();
+	if(Error!=NO_ERROR){		
+	printf("ERROR in response for GO_TO_IDLE_STATE command\n");
+	return (1);
+	}	
+	//printf("SUCCESS for GO_TO_IDLE_STATE command\n\n");					
+	
+	Error=SEND_OP_CONDITIONS();
+	if(Error!=NO_ERROR){		
+	printf("ERROR in response for SEND_OP_CONDITIONS command\n");
+	return (1);
+	}	
+	//printf("SUCCESS for SEND_OP_CONDITIONS command\n\n");					
+	printf("MMC INITIALISED\n\n");
 
-int fac (int n)
-{
-  if (n == 0)
-     return 1;
-  else
-     return (n * fac (n-1));
+	Error=SEND_READ_CSD_CMD(8);
+	if(Error!=NO_ERROR){		
+	printf("ERROR in response for SEND_READ_CSD command\n");
+	return (1);
+	}	
+	//printf("SUCCESS for SEND_READ_CSD command\n\n");
+	
+	Error=Retrive_CSD_parameters();
+	if(Error!=NO_ERROR){		
+	printf("CSD parameters not retrived\n");
+	return (1);
+	}	
+
+	Transaction_speed=Cal_Trans_speed();
+	if(Transaction_speed<20000)	// check if the Transfer speed required by the card is less than 20MHz
+	{printf("MMC card with a transfer speeds less than 20Mhz are not supported");
+	}	
+	
+	Frequency_MMC=HIGH;				// set the frequency to 20MHz
+	
+	Access_time=Cal_Access_time(20);
+	if(Access_time>150000){
+	printf("MMC card with access times greater than 150000 clock cycles are not supported");
+	}	
+	
+	Error=SET_BLOCK_LEN_CMD();
+	if(Error!=NO_ERROR){		
+	printf("ERROR in response for SET_BLOCK_LEN_CMD command\n");
+	return (1);
+	}	
+
+	Error=SEND_READ_BLOCK_CMD(10);
+	if(Error!=NO_ERROR){		
+	printf("ERROR in response for SEND_READ_BLOCK_CMD command\n");
+	return (1);
+	}	
+
+	printf("Read complete\n");	
+
+volatile char i = 1;
+	
+	while(i);
+	return(0);
 }
 
-
-int main (void)
-{
-  int i;
-  int s = 0;
-  volatile int n;
-
-  n = 5;
-  for (i = 0;  i <= n; i++)
-   	s += fac (i);
-
-#ifdef DEBUG
-  printf("Sum of factorials from [0, %d] is %d\n", n, s );
-#endif
-  END_SIM;
-  return (s);
+void intialize_global_parameters(){
+	*Readflag=1;
+	*Writeflag=0;
+	Frequency_MMC=LOW;
 }
-
--- soft/fac/thread1.c
+++ soft/fac/thread1.c
@@ -1,4 +1,86 @@
+#include"mmc_response.c"
+void Wait_for_delay(unsigned int);
+void Intialize_Read_block();
 
 int main() {
-   return 0;
+//////////////////////////////////////
+/*volatile loop = 1;
+printf("Going to infinite loop 1\n");
+while(loop == 1){
+
+
+DEAD(50);
+DEAD(00);
+//printf("thread0\n");
 }
+*/
+/////////////////////////////////////
+
+	Intialize_Read_block();	
+	Read_CMD();
+	if(Equal_buffers(CMD0)!=0){	
+		printf("Error in GO_TO_IDLE_STATE command \n\n");
+		return (1);
+	}
+	Wait_for_delay(10);
+	Send_Response(0x01);
+
+	Read_CMD();
+	if(Equal_buffers(CMD1)!=0){	
+		printf("Error in SEND_OP_CONDITIONS command \n");
+		return (1);
+	}
+	Wait_for_delay(10);
+	Send_Response(0x00);
+	
+   Read_CMD();
+	if(Equal_buffers(CMD9)!=0){	
+		printf("Error in READ_CSD command \n");
+		return (1);
+	}
+	Wait_for_delay(5);
+	Send_Response(0xFE);
+	Wait_for_delay(2);
+	Send_CSD();
+	
+	Read_CMD();
+	if(Equal_buffers(CMD16)!=0){	
+		printf("Error in Set_block_len command \n");
+		return (1);
+	}
+	Wait_for_delay(3);
+	Send_Response(0x00);
+
+	Read_CMD();
+	if(Equal_buffers(CMD17)!=0){	
+		printf("Error in READ_block command \n");
+		return (1);
+	}
+	Send_Response(0xFE);
+	Send_Read_array();
+
+printf("Write Complete\n");
+
+volatile char i = 1;
+
+	while(i);
+	return (0);
+	
+}
+
+Wait_for_delay(unsigned int count){
+	int i;		
+	for(i=0;i<count;i++){
+	delay();	
+	}		
+}
+
+void Intialize_Read_block(){
+	volatile int i;	
+	for(i=0;i<512;i++){
+		Read_array[i]=i;
+	}
+
+printf("Read_array[%d] = %d\n",i-1,Read_array[i-1]);
+
+}		
