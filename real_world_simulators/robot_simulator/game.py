5648096fc5798c9739ffa18a3a3366af53199e97
blickly <blickly@2fe1bebc-546c-4154-8a90-0a651ce2c4e0>
2009-03-21 00:29:12 +0000

    Added first swipe of a super simple robot simulator
    git-svn-id: svn+ssh://source.eecs.berkeley.edu/home/svn/chess/pret/trunk/pret_iss@1692 2fe1bebc-546c-4154-8a90-0a651ce2c4e0

---
+++ real_world_simulators/robot_simulator/game.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import os,sys
+import pygame
+from pygame.locals import *
+
+import simulator
+
+class Game:
+  """Game - This class interfaces simulator to pygame."""
+    
+  def __init__(self, width=640,height=480):
+    """Initialize screen"""
+    pygame.init()
+    self.width = width
+    self.height = height
+    self.screen = pygame.display.set_mode((self.width, self.height))
+
+    self.sim = simulator.Simulator()
+
+  def MainLoop(self):
+    """Run the main loop"""
+    self.background = pygame.Surface(self.screen.get_size())
+    self.background = self.background.convert()
+    self.background.fill((0,0,0))
+    self.time = 0
+    while 1:
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+          sys.exit()
+        elif event.type == KEYDOWN:
+          if   event.key == K_LEFT:
+            self.sim.get_robot().steer_left()
+          elif event.key == K_RIGHT:
+            self.sim.get_robot().steer_right()
+          elif event.key == K_UP:
+            self.sim.get_robot().speed_up()
+          elif event.key == K_DOWN:
+            self.sim.get_robot().slow_down()
+
+      self.screen.blit(self.background, (0, 0)) 
+      if pygame.font:
+        font = pygame.font.Font(None, 36)
+        text = font.render("Time %s" % self.sim.get_time(), 1, (255, 0, 0))
+        textpos = text.get_rect(x=300)
+        self.screen.blit(text, textpos)
+
+      pygame.draw.circle(self.screen, pygame.color.Color("white"), 
+          (self.sim.get_robot().get_x(), self.sim.get_robot().get_y()), 10)
+      pygame.display.flip()
+      self.sim.increment_time()
+
+if __name__ == "__main__":
+    window = Game()
+    window.MainLoop()
---
+++ real_world_simulators/robot_simulator/simulator.py
@@ -0,0 +1,74 @@
+import math
+
+class Simulator:
+  """Simulator - This class provides a simulation of our robot example"""
+
+  def __init__(self):
+    """Initialize time and a single robot"""
+    self.time = 0
+    self.robot = Robot()
+
+  def get_robot(self):
+    """Get a given robot"""
+    return self.robot
+
+  def get_time(self):
+    """Get current simulation time"""
+    return self.time
+
+  def increment_time(self):
+    """Increment simulation time"""
+    self.time = self.time + 1
+    self.robot.increment_time()
+
+class Robot:
+  """Robot - This class provides a robot abstraction"""
+
+  def __init__(self):
+    """Initialize robot parameters"""
+    #self.time = 0
+    self.x = 20
+    self.y = 20
+    self.speed = 0
+    self.heading = 0
+    self.wheel_dir = 0
+    self.max_wheel_angle = 0.1
+    self.wheel_increment = 0.005
+    self.speed_increment = 0.05
+ 
+
+  def steer_left(self):
+    """Move steering to the left"""
+    self.wheel_dir = self.wheel_dir + self.wheel_increment
+    if self.wheel_dir > self.max_wheel_angle:
+      self.wheel_dir = self.max_wheel_angle
+
+  def steer_right(self):
+    """Move steering to the right"""
+    self.wheel_dir = self.wheel_dir - self.wheel_increment
+    if self.wheel_dir < -self.max_wheel_angle:
+      self.wheel_dir = -self.max_wheel_angle
+
+  def speed_up(self):
+    """Accelerate"""
+    self.speed = self.speed + self.speed_increment
+
+  def slow_down(self):
+    """Decelerate"""
+    if self.speed > 0:
+      self.speed = self.speed - self.speed_increment
+
+  def get_x(self):
+    """Get x coordinate"""
+    return self.x
+
+  def get_y(self):
+    """Get y coordinate"""
+    return self.y
+
+  def increment_time(self):
+    """Move the robot by a single time unit"""
+    #self.time = self.time + 1
+    self.heading = self.heading + self.wheel_dir
+    self.x = self.x + math.sin(self.heading) * self.speed
+    self.y = self.y + math.cos(self.heading) * self.speed
