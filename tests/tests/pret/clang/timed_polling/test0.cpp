93b1274477a9750d3b77c5876d284b0d9d2e67b1
bldmastr <bldmastr@2fe1bebc-546c-4154-8a90-0a651ce2c4e0>
2009-03-23 02:02:05 +0000

    fixed a dump error
    git-svn-id: svn+ssh://source.eecs.berkeley.edu/home/svn/chess/pret/trunk/pret_iss@1696 2fe1bebc-546c-4154-8a90-0a651ce2c4e0

--- tests/tests/pret/clang/timed_polling/test0.cpp
+++ tests/tests/pret/clang/timed_polling/test0.cpp
@@ -79,7 +79,7 @@ int main(int argc, char *argv[])
         }
         /* Generate the SREC files*/
         string compile_srec = pret_iss_path + "/scripts/compile_threads.py " + load_file.c_str() + " -q";
-        compile_srec = compile_srec + " &2>/dev/null";
+        //compile_srec = compile_srec + " &2>/dev/null";
 cout << "compile command: " << compile_srec << endl;
         int failed = system(compile_srec.c_str());
 	if (failed) {
