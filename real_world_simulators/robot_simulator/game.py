80eb88ba1fa17dbc2adf6ba8fdc718b92a7b1286
blickly <blickly@2fe1bebc-546c-4154-8a90-0a651ce2c4e0>
2009-03-24 02:54:10 +0000

    Factored control and display into separate methods
    git-svn-id: svn+ssh://source.eecs.berkeley.edu/home/svn/chess/pret/trunk/pret_iss@1711 2fe1bebc-546c-4154-8a90-0a651ce2c4e0

--- real_world_simulators/robot_simulator/game.py
+++ real_world_simulators/robot_simulator/game.py
@@ -19,53 +19,60 @@ class Game:
 
     self.sim = simulator.Simulator(width / boxsize, self.height / boxsize)
 
+  def keyboard_control(self):
+    """Do keyboard-based control"""
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT: 
+        sys.exit()
+      elif event.type == KEYDOWN:
+        for robot in self.sim.get_robots():
+          if event.key == K_LEFT:
+            robot.steer_left()
+          elif event.key == K_RIGHT:
+            robot.steer_right()
+          elif event.key == K_UP:
+            robot.speed_up()
+          elif event.key == K_DOWN:
+            robot.slow_down()
+
+  def display(self):
+    """Display current simulation configuration graphically"""
+    # Clear screen
+    self.screen.blit(self.background, (0, 0))
+
+    # Display walls
+    for x in xrange(self.width / self.boxsize):
+      for y in xrange(self.height / self.boxsize):
+        box = self.sim.read_grid(x,y)
+        if box == simulator.Simulator.wall:
+          pygame.draw.circle(self.screen, pygame.color.Color("gray"),
+                             (self.boxsize*x, self.height - self.boxsize*y),
+                             self.boxsize/2)
+    # Print time
+    if pygame.font:
+      font = pygame.font.Font(None, 36)
+      text = font.render("Time %d" % self.sim.get_time(), 1, (255, 0, 0))
+      textpos = text.get_rect(x=self.width-200)
+      self.screen.blit(text, textpos)
+
+    # Display robot(s)
+    for robot in self.sim.get_robots():
+      pygame.draw.circle(self.screen, pygame.color.Color("white"),
+                 (robot.get_x() * self.boxsize,
+                  self.height - robot.get_y() * self.boxsize),
+                         self.boxsize/2)
+
+    pygame.display.flip()
+
   def MainLoop(self):
     """Run the main loop"""
-
     self.background = pygame.Surface(self.screen.get_size())
     self.background = self.background.convert()
     self.background.fill((0,0,0))
     self.time = 0
     while 1:
-      for event in pygame.event.get():
-        if event.type == pygame.QUIT: 
-          sys.exit()
-        elif event.type == KEYDOWN:
-          for robot in self.sim.get_robots():
-            if event.key == K_LEFT:
-              robot.steer_left()
-            elif event.key == K_RIGHT:
-              robot.steer_right()
-            elif event.key == K_UP:
-              robot.speed_up()
-            elif event.key == K_DOWN:
-              robot.slow_down()
-
-      self.screen.blit(self.background, (0, 0)) 
-
-      # Display walls
-      for x in xrange(self.width / self.boxsize):
-        for y in xrange(self.height / self.boxsize):
-          box = self.sim.read_grid(x,y)
-          if box == simulator.Simulator.wall:
-            pygame.draw.circle(self.screen, pygame.color.Color("gray"),
-                               (self.boxsize*x, self.height - self.boxsize*y), 
-                               self.boxsize/2)
-      # Print time
-      if pygame.font:
-        font = pygame.font.Font(None, 36)
-        text = font.render("Time %d" % self.sim.get_time(), 1, (255, 0, 0))
-        textpos = text.get_rect(x=self.width-200)
-        self.screen.blit(text, textpos)
-
-      # Display robot(s)
-      for robot in self.sim.get_robots():
-        pygame.draw.circle(self.screen, pygame.color.Color("white"),
-                   (robot.get_x() * self.boxsize, 
-                    self.height - robot.get_y() * self.boxsize), 
-                           self.boxsize/2)
-
-      pygame.display.flip()
+      self.keyboard_control()
+      self.display()
       self.sim.increment_time()
 
 if __name__ == "__main__":
