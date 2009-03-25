a8c3587086e403474ae4e78cf91144ad5f9aefcb
blickly <blickly@2fe1bebc-546c-4154-8a90-0a651ce2c4e0>
2009-03-25 02:59:18 +0000

    Made example slightly more advanced using DMAMV
    git-svn-id: svn+ssh://source.eecs.berkeley.edu/home/svn/chess/pret/trunk/pret_iss@1729 2fe1bebc-546c-4154-8a90-0a651ce2c4e0

--- real_world_simulators/robot_simulator/back_and_forth/all_threads.c
+++ real_world_simulators/robot_simulator/back_and_forth/all_threads.c
@@ -8,29 +8,45 @@
 #define WHEEL ((volatile int*) 0x80000400)
 enum direction {LEFT = 1, RIGHT = 2};
 
-#define SPEED ((volatile int*) 0x80000404)
-enum speed {STOP = 1, GO = 2};
+#define MOTOR ((volatile int*) 0x80000404)
+enum motor {STOP = 1, GO = 2};
+
+void uturnleft() {
+   *WHEEL = LEFT; *WHEEL = LEFT;
+}
+
+void uturnright() {
+   *WHEEL = RIGHT; *WHEEL = RIGHT;
+}
 
 int main() {
 #if defined(THREAD_0)
+  int addr;
+  for (addr = &uturnleft; addr < &main; addr += 16) {
+    DMAMV(addr, addr % 0x1000);
+  }
+
   enum {L, R} state;
-  *SPEED = GO;
+  *MOTOR = GO;
   state = R;
+
   for (;;) {
     switch (state) {
       case L:
         while (*X_COORD > BLOCK_LENGTH * 4);
-        *WHEEL = RIGHT; *WHEEL = RIGHT;
+        uturnright();
         state = R;
         break;
       case R:
-        while (*X_COORD < BLOCK_LENGTH * 10);
-        *WHEEL = LEFT; *WHEEL = LEFT;
+        while (*X_COORD < BLOCK_LENGTH * 5);
+        uturnleft();
         state = L;
         break;
     }
   }
-  *SPEED = STOP;
+  *MOTOR = STOP;
 #endif
   WAIT_FOR_END_SIMULATION;
 }
+
+void sentinal() {}
