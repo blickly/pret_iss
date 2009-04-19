07e04b8e1aa8bb97a400dc2da4967d4e2414fe61
blickly <blickly@2fe1bebc-546c-4154-8a90-0a651ce2c4e0>
2009-04-19 02:02:40 +0000

    Changed control_program example so that it uses deadlines.
    git-svn-id: svn+ssh://source.eecs.berkeley.edu/home/svn/chess/pret/trunk/pret_iss@1792 2fe1bebc-546c-4154-8a90-0a651ce2c4e0

--- real_world_simulators/robot_simulator/control_program/all_threads.c
+++ real_world_simulators/robot_simulator/control_program/all_threads.c
@@ -11,10 +11,17 @@ enum speed {STOP = 1, GO = 2};
 int main() {
 #if defined(THREAD_0)
   int i;
+  *WHEEL = LEFT;
   *SPEED = GO;
+  // Robot goes 1 square in 100,000 clock cycles.
+  // On a virtual processor (1/6th the speed) this translates
+  // to a wait of 16,666 2/3 or ~16667 cycles
+  DEADLOAD(16667);
   for (i = 0; i < 10; i++) {
-    *WHEEL = LEFT;
+    DEAD(16667);
     *WHEEL = RIGHT;
+    DEAD(16667);
+    *WHEEL = LEFT;
   }
   *SPEED = STOP;
 #else
